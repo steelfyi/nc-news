b731824332ed6b81c2fcb17a9dc0f54469cfa84d
James <jamesnixonsteel@gmail.com>
2023-03-14 13:31:02 +0000

    added functionality to view a list of comments from the related article

--- src/components/ArticleCard.jsx
+++ src/components/ArticleCard.jsx
@@ -1,17 +1,21 @@
 import { useEffect, useState } from "react";
 import { fetchArticleByID } from "../api";
 import { useParams } from "react-router-dom";
+import CommentList from "./CommentList";
 import {
   Card,
   CardHeader,
   CardContent,
   Typography,
   CardMedia,
+  Fab,
 } from "@mui/material";
+import CommentRoundedIcon from "@mui/icons-material/CommentRounded";
 
 function ArticleCard() {
   const [articleByID, setArticleByID] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [showComments, setShowComments] = useState(false);
   const { article_id } = useParams();
 
   useEffect(() => {
@@ -26,10 +30,14 @@ function ArticleCard() {
     return <h3>Loading...</h3>;
   }
 
+  const handleCommentClick = () => {
+    setShowComments(!showComments);
+  };
+
   return (
     <>
       {articleByID.map((article) => (
-        <Card key={article.article_id}>
+        <Card key={article.article_id} className="article-card">
           <CardHeader title={article.title} />
           <CardMedia image={article.article_img_url} title={article.title} />
           <CardContent>
@@ -45,6 +53,17 @@ function ArticleCard() {
           </CardContent>
         </Card>
       ))}
+      <CommentRoundedIcon
+        className="comment-icon"
+        sx={{
+          fontSize: "2.5rem",
+          color: "#f00",
+          "&:hover": { color: "#c00" },
+          cursor: "pointer",
+        }}
+        onClick={handleCommentClick}
+      />
+      {showComments && <CommentList />}
     </>
   );
 }
